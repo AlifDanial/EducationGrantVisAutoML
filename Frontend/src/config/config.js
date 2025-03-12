750cf3051ab59960cdb92ab9c49e03591af87fc6
Muhammad Alif Danial <aldmnsri@gmail.com>
2025-03-12 15:09:23 +0800

    Ngrok Deployed

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = "https://5b0e-188-43-14-13.ngrok-free.app:8080";
-// export const BACKEND_BASE_URL = "https://5b0e-188-43-14-13.ngrok-free.app:8080/api/";
+export const API_BASE_URL = "https://0703-14-192-210-146.ngrok-free.app:8000";
+export const BACKEND_BASE_URL = "https://0703-14-192-210-146.ngrok-free.app:8000/api/";
 
-export const API_BASE_URL = "http://localhost:8000";
-export const BACKEND_BASE_URL = "http://localhost:8000/api/";
+// export const API_BASE_URL = "http://localhost:8000";
+// export const BACKEND_BASE_URL = "http://localhost:8000/api/";
